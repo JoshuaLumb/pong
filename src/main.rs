29785a57159db302cf70fde0fa847b5a2122bfd9
JoshuaLumb <joshua.lumb@gmail.com>
2020-07-14 20:26:18 -0400

    Comments for understanding

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ const WINDOW_HEIGHT: f32 = 480.0;
 const PADDLE_SPEED: f32 = 8.0;
 
 struct GameState {
+    /* GameState is a struct implementing Tetra's State trait.
+     * Here we'll hold relevant values for stateful game elements. */
     paddle_texture: Texture,
     paddle_position: Vec2<f32>
 }
@@ -15,8 +17,7 @@ struct GameState {
 impl GameState{
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         let paddle_texture = Texture::new(ctx, "./resources/player1.png")?;
-        let paddle_position =
-            Vec2::new(16.0, (WINDOW_HEIGHT - paddle_texture.height() as f32) / 2.0);
+        let paddle_position = Vec2::new(16.0, (WINDOW_HEIGHT - paddle_texture.height() as f32) / 2.0);
         Ok(GameState {
             paddle_texture,
             paddle_position,
@@ -36,6 +37,9 @@ impl State for GameState {
     }
 
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        // Tetra updates state 60 times per second by default.
+
+        //Handle inputs for player 1
         if input::is_key_down(ctx, Key::W) && self.paddle_position.y > 0.0 {
             self.paddle_position.y -= PADDLE_SPEED;
         }
@@ -48,6 +52,8 @@ impl State for GameState {
 }
 
 fn main() -> tetra::Result {
+    /* Establish a context - Tetra uses this to hold global state info like window settings
+     * and connections to graphics/audio/input hardware. */
     ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
         .quit_on_escape(true)
         .build()?
