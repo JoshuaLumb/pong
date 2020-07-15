eaffd3a577ae10a1c8bb90f189c74c23f457ec49
JoshuaLumb <joshua.lumb@gmail.com>
2020-07-14 21:04:43 -0400

    Add the ball and a new "velocity" component for entities

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Rectangle, Texture};
 use tetra::input::{self, Key};
 use tetra::math::Vec2;
+use tetra::window;
 use tetra::{Context, ContextBuilder, State};
 
 const WINDOW_WIDTH: f32 = 640.0;
 const WINDOW_HEIGHT: f32 = 480.0;
 const PADDLE_SPEED: f32 = 8.0;
+const BALL_SPEED: f32 = 5.0;
 
 struct Entity {
     // An entity here will be any stateful object in the game
     texture: Texture,
     position: Vec2<f32>,
+    velocity: Vec2<f32>,
 }
 
 impl Entity {
     // Entities will have a texture and a position on the map
     fn new(texture: Texture, position: Vec2<f32>) -> Entity {
-        Entity {texture, position}
+        Entity::with_velocity(texture, position, Vec2::zero())
+    }
+
+    fn with_velocity(texture: Texture, position: Vec2<f32>, velocity: Vec2<f32>) -> Entity {
+        Entity {
+            texture,
+            position,
+            velocity,
+        }
     }
 }
 
@@ -25,6 +36,7 @@ struct GameState {
      * Here we'll hold relevant values for stateful game elements. */
     player1: Entity,
     player2: Entity,
+    ball: Entity,
 }
 
 impl GameState{
@@ -41,9 +53,18 @@ impl GameState{
             (WINDOW_HEIGHT - player2_texture.height() as f32) / 2.0,
         );
 
+        let ball_texture = Texture::new(ctx, "./resources/ball.png")?;
+        let ball_position = Vec2::new(
+            WINDOW_WIDTH / 2.0 - ball_texture.width() as f32 / 2.0,
+            WINDOW_HEIGHT /2.0 - ball_texture.height() as f32 / 2.0,
+        );
+
+        let ball_velocity = Vec2::new(-BALL_SPEED, 0.0);
+
         Ok(GameState {
             player1: Entity::new(player1_texture, player1_position),
             player2: Entity::new(player2_texture, player2_position),
+            ball: Entity::with_velocity(ball_texture, ball_position, ball_velocity),
         })
     }
 }
@@ -56,6 +77,7 @@ impl State for GameState {
         //Draw player paddles
         graphics::draw(ctx, &self.player1.texture, self.player1.position);
         graphics::draw(ctx, &self.player2.texture, self.player2.position);
+        graphics::draw(ctx, &self.ball.texture, self.ball.position);
 
         Ok(())
     }
@@ -77,6 +99,9 @@ impl State for GameState {
         if input::is_key_down(ctx, Key::Down) && self.player2.position.y < (WINDOW_HEIGHT - self.player2.texture.height() as f32) {
             self.player2.position.y += PADDLE_SPEED;
         }
+
+        // Handle ball position
+        self.ball.position += self.ball.velocity;
 
         Ok(())
     }
